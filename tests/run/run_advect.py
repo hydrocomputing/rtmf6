a635c78b0efc866655a4ac48bfc5187f4891066d
Mike Müller <mmueller@python-academy.de>
2024-05-02 22:55:04 +0200

    Add phreeqcrm calculations

--- tests/run/run_advect.py
+++ tests/run/run_advect.py
@@ -7,8 +7,12 @@ from rtmf6.run import main
 
 def run(model_name):
     model_path = Path(__file__).parent / f'../models/{model_name}'
-    main(model_path)
+    phreeqcrm_yaml = Path(__file__).parent / '../phreeqpy/advect_cpp_exception.yaml'
+    main(model_path, phreeqcrm_yaml, reactions=True)
 
 if __name__ == '__main__':
+    from timeit import default_timer
+    start = default_timer()
     mp.set_start_method('spawn')
     run('advect')
+    print(default_timer() - start)
