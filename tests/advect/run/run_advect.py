115a00683fc22372fb67d15dc4a3f0ef1d25a58a
Mike Müller <mmueller@python-academy.de>
2024-05-05 10:39:28 +0200

    Run with json data as input

--- tests/advect/run/run_advect.py
+++ tests/advect/run/run_advect.py
@@ -1,18 +1,25 @@
 """Run model advect."""
 
+import json
 import multiprocessing as mp
 from pathlib import Path
+import sys
+from timeit import default_timer
+
 
 from rtmf6.run import main
 
-def run(model_name):
+def run_model(model_name):
     model_path = Path(__file__).parent / f'../models/{model_name}'
     phreeqcrm_yaml = model_path / f'{model_name}.yaml'
     main(model_path, phreeqcrm_yaml, reactions=True)
 
-if __name__ == '__main__':
-    from timeit import default_timer
+def run():
     start = default_timer()
     mp.set_start_method('spawn')
-    run('advect7')
+    specific_model_data = json.loads(Path(sys.argv[1]).read_text())
+    run_model(model_name=specific_model_data['name'])
     print(default_timer() - start)
+
+if __name__ == '__main__':
+    run()
