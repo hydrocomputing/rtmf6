8bd68f0bff51cbb3220b08f2b5b817677d66e9c4
Mike Müller <mmueller@python-academy.de>
2024-05-05 10:40:43 +0200

    Move model config to json file

--- tests/advect/model_creation/run_all_steps.py
+++ tests/advect/model_creation/run_all_steps.py
@@ -6,7 +6,10 @@ Run all steps to create a model.
 3. Create PhreeqcRM yaml file.
 """
 
+import json
 from pathlib import Path
+import sys
+
 from make_base_model import make_input_data
 from clone_model import make_all_component_models
 from create_yaml import create_yaml
@@ -31,15 +34,27 @@ from create_yaml import create_yaml
 MODEL_NAME = 'advect7'
 GEOMETRY = dict(nlay=1, nrow=1, ncol=100, delr=1.0, delc=1.0)
 
-BASE_PATH = Path(__file__).parent.parent
-MODEL_PATH = BASE_PATH / 'models' / MODEL_NAME
-YAML_PATH = MODEL_PATH / f'{MODEL_NAME}.yaml'
-PHREEQCRM_DATA_PATH = BASE_PATH / 'phreeqcrm_data'
+
+def do_steps(specific_model_data, base_path=None):
+    """Do all preprocessing steps."""
+
+    model_name = specific_model_data['name']
+    geometry = specific_model_data['geometry']
+    if base_path is None:
+        base_path = Path(__file__).parent.parent
+    models_path= base_path / 'models'
+    model_path = models_path / model_name
+    yaml_path = model_path / f'{model_name}.yaml'
+    phreeqcrm_data_path = base_path / 'phreeqcrm_data'
 
 
-def do_steps(model_path=MODEL_PATH, model_name=MODEL_NAME, geometry=GEOMETRY):
+    models_path.mkdir(exist_ok=True)
     model_path.mkdir(exist_ok=True)
-    base_model_path = make_input_data(model_path, model_name=model_name, geometry=geometry)
+    print('making base model')
+    base_model_path = make_input_data(
+        model_path, model_name, specific_model_data)
+    print('making component models')
+    print(base_model_path)
     make_all_component_models(
         model_path=base_model_path,
         model_name=model_name,
@@ -48,11 +63,17 @@ def do_steps(model_path=MODEL_PATH, model_name=MODEL_NAME, geometry=GEOMETRY):
     nxyz = geometry['nlay'] * geometry['nrow'] * geometry['ncol']
     print(nxyz)
     create_yaml(
-        file_name=YAML_PATH,
-        data_path=PHREEQCRM_DATA_PATH,
+        file_name=yaml_path,
+        data_path=phreeqcrm_data_path,
         nxyz=nxyz,
         )
 
 
+def main():
+    """Run all steps."""
+    specific_model_data = json.loads(Path(sys.argv[1]).read_text())
+    do_steps(specific_model_data)
+
+
 if __name__ == '__main__':
-    do_steps()
+    main()
