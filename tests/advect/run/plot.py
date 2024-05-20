d3c090af0ca29271ee27ecd05baecaefa955e454
Mike Müller <mmueller@python-academy.de>
2024-05-20 08:49:52 -0400

    Multiply concentrations by 1,000 mol --> mmol/kgw

--- tests/advect/run/plot.py
+++ tests/advect/run/plot.py
@@ -9,13 +9,14 @@ def plot(model_name, components=('Na', 'Cl', 'K', 'Ca')):
     for path in component_models_dir.glob('*'):
         name = path.name
         if name in components:
-            value = pd.read_csv(path / f'gwt_{model_name}.obs.csv')[['time',  'CENTER_LAYER_0']]
+            value = pd.read_csv(path / f'gwt_{model_name}.obs.csv')[['time', 'CENTER_LAYER_0']]
             value.columns = ['time', name]
+            value[name] *= 1_000
             conc[name] = value
     res = conc[components[0]]
     for name in components[1:]:
         res = pd.merge(res, conc[name])
-    ax = res.plot(x='time')
+    res.plot(x='time')
 
 
 if __name__ == '__main__':
