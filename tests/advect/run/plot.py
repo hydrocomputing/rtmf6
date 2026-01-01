851c63cd60aad03abbf14f985f5cecdf9a20f570
Mike Müller <mmueller@python-academy.de>
2026-01-01 18:48:16 +0100

    Add plot time step

--- tests/advect/run/plot.py
+++ tests/advect/run/plot.py
@@ -2,22 +2,25 @@ from pathlib import Path
 
 import pandas as pd
 
-def plot(model_name, components=('Na', 'Cl', 'K', 'Ca')):
+def plot(model_name, components=('Na', 'Cl', 'K', 'Ca'), steps=None):
     """Plot concentrations."""
     component_models_dir = Path(__file__).parent / f'../models/{model_name}' / 'component_models'
     conc = {}
     for path in component_models_dir.glob('*'):
         name = path.name
         if name in components:
-            value = pd.read_csv(path / f'gwt_{model_name}.obs.csv')[['time', 'CENTER_LAYER_0']]
+            value = pd.read_csv(
+                path / f'gwt_{model_name}.obs.csv',
+                nrows=steps,
+                )[['time', 'CENTER_LAYER_0']]
             value.columns = ['time', name]
             value[name] *= 1_000
             conc[name] = value
     res = conc[components[0]]
     for name in components[1:]:
         res = pd.merge(res, conc[name])
-    res.plot(x='time')
+    return res.plot(x='time', ylabel='mmols/kgw')
 
 
 if __name__ == '__main__':
-    plot('advect1')
+    plot('advect100r').figure.savefig('100.png')
