b886bb2e62c8127dc1a56a04e4336ab1db68b11b
Mike Müller <mmueller@python-academy.de>
2026-01-17 21:28:42 +0100

    Add first version of documentation

---
+++ docs/source/benchmarks/cation_exchange_1d/cat_ion_exchange_1d.md
@@ -0,0 +1,10 @@
+# Cat Ion Exchange 1D
+
+
+```{toctree}
+:maxdepth: 2
+
+
+MakeMF6Model.ipynb
+Results.ipynb
+```
---
+++ docs/source/conf.py
@@ -0,0 +1,132 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys
+from pathlib import Path
+
+# Add the source directory to the path for autodoc
+sys.path.insert(0, str(Path(__file__).parents[2] / "src"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "rtmf6"
+copyright = "2024, Mike Müller"
+author = "Mike Müller"
+
+# The version info for the project
+try:
+    from rtmf6 import __version__
+
+    version = __version__
+    release = __version__
+except ImportError:
+    version = "0.0.1"
+    release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # Core Sphinx extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    # Jupyter notebook support (includes myst_parser functionality)
+    "myst_nb",
+    # Type hints in autodoc
+    "sphinx_autodoc_typehints",
+    # UI enhancements
+    "sphinx_copybutton",
+    "sphinx_design",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# The suffix(es) of source filenames.
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "myst-nb",
+    ".myst": "myst-nb",
+    ".ipynb": "myst-nb",
+}
+
+# -- Options for MyST --------------------------------------------------------
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+# -- Options for myst-nb -----------------------------------------------------
+
+nb_execution_mode = "off"
+nb_execution_timeout = 120
+nb_execution_raise_on_error = True
+
+# -- Options for autodoc -----------------------------------------------------
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+}
+
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+
+# -- Options for autosummary -------------------------------------------------
+
+autosummary_generate = True
+
+# -- Options for intersphinx -------------------------------------------------
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# Furo theme options
+html_theme_options = {
+    "source_repository": "https://github.com/your-org/rtmf6",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+    "light_css_variables": {
+        "color-brand-primary": "#2962ff",
+        "color-brand-content": "#2962ff",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#82b1ff",
+        "color-brand-content": "#82b1ff",
+    },
+}
+
+# -- Options for copybutton --------------------------------------------------
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: "
+copybutton_prompt_is_regexp = True
