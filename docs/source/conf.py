9a50a9881cc92eef1156959508675cb1ff217149
Mike Müller <mmueller@python-academy.de>
2026-03-14 16:46:20 +0100

    Fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, str(Path(__file__).parents[2] / "src"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "rtmf6"
-copyright = "2024- 2026, Mike Müller"
+copyright = "2024-2026, Mike Müller"
 author = "Mike Müller"
 
 # The version info for the project
